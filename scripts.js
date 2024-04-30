643efb92182e182b1d50b393bec25f3d7613fa0a
SbabalweRoux <sbaviaroux@gmail.com>
2024-04-30 16:21:58 +0200

    Fixed error and added some error handling

--- scripts.js
+++ scripts.js
@@ -5,5 +5,28 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = dividend / divider;
+
+  //Input validation
+  if (isNaN(dividend) || isNaN(divider)) {
+    document.body.innerHTML = "Something critical went wrong. Please reload the page.";
+    console.error("Invalid input: Non-numeric values provided");
+    return;
+  }
+
+  //Error handling
+  if (divider === "0") {
+    result.innerText = "Division not performed. Invalid number provided. Try again.";
+    console.error("Invalid division: Division by zero");
+    return;
+  }
+
+  //Display result in a case of no input
+  if (dividend === "" || divider === "") {
+    result.innerText = "No calculation performed";
+    return;
+  }
+
+  //Result display
+  const divisionResult = Math.floor(dividend / divider);
+  result.innerText = divisionResult;
 });
